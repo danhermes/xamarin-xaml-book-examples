bafc4f646f4048e40b8161a49b8bb6e6c1961dcb
Dan Hermes <dan@lexiconsystemsinc.com>
2018-06-07 01:12:51 -0400

    Ch. 3 FlexLayout Example

---
+++ Chapter 3/LayoutExample.Xaml/LayoutExample.Xaml/Views/FlexLayoutExample.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutExample.Xaml.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FlexLayoutExample : ContentPage
+	{
+		public FlexLayoutExample ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
