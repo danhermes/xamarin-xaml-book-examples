26596cc0b4c83c7a461618cecc636324c72581f6
Dan Hermes <dan@lexiconsystemsinc.com>
2018-10-14 01:48:12 -0400

    Add App.xaml and remove old NavDraw

---
+++ Chapter 7/NavigationExamples.Xaml/NavigationExamples.Xaml/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavigationExamples
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent ();
+            MainPage = new NavigationDrawer();
+        }
+	}
+}
